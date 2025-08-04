be977276b58a609363ad1e78e6b0d8d7ec6c0314
daysite <danieldevelop3@gmail.com>
2025-08-04 00:42:23 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -658,7 +658,7 @@ global.dfail = (type, m, conn) => {
     
     let setn = global.db.data.settings[conn.user.jid] || {}
     let botnombrexd = setn.botName
-    let deco_msg = "`१✿ᩧ┅═❏✧͚"+ setn.botName +"❏═┅✿ᩧ̼१`\n\n"+`${msg}\n\n`+"`︶ִֶָ⏝︶ִֶָ⏝˖ ࣪ ୨✧୧ ࣪ ˖⏝ִֶָ︶⏝ִֶָ︶`"
+    //let deco_msg = "`१✿ᩧ┅═❏✧͚"+ setn.botName +"❏═┅✿ᩧ̼१`\n\n"+`${msg}\n\n`+"`︶ִֶָ⏝︶ִֶָ⏝˖ ࣪ ୨✧୧ ࣪ ˖⏝ִֶָ︶⏝ִֶָ︶`"
     if (msg) return conn.reply(m.chat, deco_msg, m)
 }
 
